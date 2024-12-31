0aeea2f640f81ed0d04f7b98dd35b373f2b6bbe8
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-31 18:27:10 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,39 @@
 const $ = <T extends HTMLElement>(id: string): T =>
   document.getElementById(id) as T
+const textarea = $<HTMLTextAreaElement>('formula')
+const prove = $<HTMLButtonElement>('prove')
 
+// set formula from url parameter to textarea
 document.addEventListener('DOMContentLoaded', () => {
   // get formula from url parameter
   const formula = new URLSearchParams(location.search).get('formula')
   if (formula) {
     // set formula to textarea
-    $<HTMLTextAreaElement>('formula').value = decodeURIComponent(formula)
+    textarea.value = decodeURIComponent(formula)
   }
 })
 
+// set event listener to each formula link that sets formula to textarea and submits form
 for (const el of document.querySelectorAll('a[data-formula]')) {
   el.addEventListener('click', () => {
     // set formula to textarea
-    $<HTMLTextAreaElement>('formula').value = el.getAttribute('data-formula')!
+    textarea.value = el.getAttribute('data-formula')!
     // submit form
-    $<HTMLButtonElement>('prove').click()
+    prove.click()
   })
 }
 
-$('prover-form').addEventListener('submit', event => {
+// set event listener to form that sends formula to server
+document.querySelector('form')!.addEventListener('submit', event => {
   event.preventDefault()
   // get formula from textarea
-  const formula = $<HTMLTextAreaElement>('formula').value
+  const formula = textarea.value
   // update url
   history.pushState({}, '', `?formula=${encodeURIComponent(formula)}`)
   // create form data
   const formData = new FormData(event.target as HTMLFormElement)
   // set aria-busy
-  $<HTMLButtonElement>('prove').setAttribute('aria-busy', 'true')
+  prove.setAttribute('aria-busy', 'true')
   // TODO: 後で消す
   formData.forEach((value, key) => {
     // biome-ignore lint/suspicious/noConsoleLog:
