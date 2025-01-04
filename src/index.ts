fce2857a6f2440bc99cd65e6f0ec5302ef3596d9
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-01-05 00:31:20 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,40 @@
-const $ = <T extends HTMLElement>(id: string): T =>
-  document.getElementById(id) as T
-const textarea = $<HTMLTextAreaElement>('formula')
-const prove = $<HTMLButtonElement>('prove')
+import Alpine from 'alpinejs'
 
-// set formula from url parameter to textarea
-document.addEventListener('DOMContentLoaded', () => {
-  // get formula from url parameter
-  const formula = new URLSearchParams(location.search).get('formula')
-  if (formula) {
-    // set formula to textarea
-    textarea.value = decodeURIComponent(formula)
-  }
-})
+Alpine.data('prover', () => ({
+  formula: '',
+  isLoading: false,
 
-// set event listener to each formula link that sets formula to textarea and submits form
-for (const el of document.querySelectorAll('a[data-formula]')) {
-  el.addEventListener('click', () => {
-    // set formula to textarea
-    textarea.value = el.getAttribute('data-formula')!
+  init() {
+    // get formula from url parameter
+    const formula = new URLSearchParams(location.search).get('formula')
+    if (formula) {
+      // set formula
+      this.formula = decodeURIComponent(formula)
+    }
+  },
+
+  async proveFormula(formula: string) {
+    // set formula
+    this.formula = formula
+    // wait for DOM update
+    await this.$nextTick()
     // submit form
-    prove.click()
-  })
-}
+    this.prove()
+  },
 
-// set event listener to form that sends formula to server
-document.querySelector('form')!.addEventListener('submit', event => {
-  event.preventDefault()
-  // get formula from textarea
-  const formula = textarea.value
-  // update url
-  history.pushState({}, '', `?formula=${encodeURIComponent(formula)}`)
-  // create form data
-  const formData = new FormData(event.target as HTMLFormElement)
-  // set aria-busy
-  prove.setAttribute('aria-busy', 'true')
-  // TODO: 後で消す
-  formData.forEach((value, key) => {
-    // biome-ignore lint/suspicious/noConsoleLog:
-    console.log(`${key}: ${value}`)
-  })
-})
+  prove() {
+    this.isLoading = true
+    // update url
+    history.pushState({}, '', `?formula=${encodeURIComponent(this.formula)}`)
+    // create form data
+    const formData = new FormData(document.querySelector('form')!)
+    // TODO: 後で消す
+    formData.forEach((value, key) => {
+      console.log(`${key}: ${value}`)
+    })
+    const lang = formData.get('lang') as string
+    this.isLoading = false
+  },
+}))
+
+Alpine.start()
